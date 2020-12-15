87e59539c8f1774c4209d865efd6d16d5bf71df3
teshome hailu <teshe@2008@hotmail.com>
2020-12-15 17:43:20 +0300

    sanctum package added

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,17 +1,49 @@
 <template>
 <div>
-    <h1>Vue Router Demo App</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <p>
-        <router-link :to="{ name: 'home' }">Home</router-link> |
-        <router-link :to="{ name: 'hello' }">Hello World</router-link>
-    </p>
-
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <router-link :to="{name: 'home'}" class="nav-link">Home <span class="sr-only">(current)</span></router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{name: 'hello'}" class="nav-link" href="#">Link</router-link>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
     <div class="container">
         <router-view></router-view>
     </div>
 </div>
 </template>
+
+<style>
+@import'~bootstrap/dist/css/bootstrap.css';
+</style>
 
 <script>
 export default {}
